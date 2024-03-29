07785c395a6c765f00a1479893f5b0094f98cb8b
PEIZHEN <peizhennie@gmail.com>
2024-03-29 21:15:01 +0800

    added pie chart to workout

--- myFitness/ClientApp/src/components/Workout/WorkoutMain.js
+++ myFitness/ClientApp/src/components/Workout/WorkoutMain.js
@@ -1,33 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { Link, TableSortLabel, Table, TableBody, TableFooter, TableContainer, TableHead, TableRow, Select, MenuItem, InputAdornment, Divider, Box, Chip, Typography, FormControl, Grid, FormGroup, TextField, Button, FormLabel, FormControlLabel, Container } from '@mui/material';
-import { useLocation, useParams, useNavigate } from "react-router-dom";
-import { BaseRoutes } from '../helper/Routing';
+import React from 'react';
+import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
+import { Typography, Box, Grid, Divider, CardContent, Card, Chip } from '@mui/material';
+
+const mockData = [
+    { name: 'Workout', value: 30 },
+    { name: 'Fitness', value: 20 },
+    { name: 'Yoga', value: 50 },
+    { name: 'Dance', value: 50 },
+
+];
+
+const COLORS = ['#FF5733', '#FFC300', '#36A2EB', '#4BC0C0', '#9966FF'];
 
 export const WorkoutMain = () => {
-    const navigate = useNavigate();
-
-
-    const handleClose = () => {
-        navigate(BaseRoutes.Workout)
-    }
-
 
     return (
         <Box sx={{ marginTop: '1rem' }}>
-            <Grid container>
-                <Typography variant="h6" sx={{ fontWeight: 'bold', marginRight: '0.5rem', fontSize: '1.5rem' }}>Workout Dashboard</Typography>
-                <Grid item md={12}>
-                    <Divider sx={{ padding: '0.5rem 0.75rem', marginBottom: '0.75rem' }} />
-
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>Workout Dashboard</Typography>
+                    <Divider />
                 </Grid>
-                <Grid item md={12}>
+                <Grid item md={6} xs={12}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>Event Statistics</Typography>
+                    <Grid container spacing={2}>
+                    <Grid item md={6}> 
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Events attended:
+                            </Typography>
+                            <Box display="flex" justifyContent="space-around" alignItems="center">
+                                <PieChart width={400} height={300}>
+                                    <Pie
+                                        data={mockData}
+                                        dataKey="value"
+                                        nameKey="name"
+                                        cx="40%"  // Adjust the center x-coordinate to give more space for the legend on the right
+                                        cy="50%"
+                                        outerRadius={100}
+                                        fill="#8884d8"
+                                    >
+                                        {mockData.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                        ))}
+                                    </Pie>
+                                    <Tooltip />
+                                    <Legend align="right" verticalAlign="middle" layout="vertical" />
+                                </PieChart>
 
+                            </Box>
+                        </CardContent>
+                    </Card></Grid>
+                    <Grid item md={6}>  
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Events attended:
+                            </Typography>
+                            <Box display="flex" justifyContent="space-around" alignItems="center">
+                                <PieChart width={400} height={300}>
+                                    <Pie
+                                        data={mockData}
+                                        dataKey="value"
+                                        nameKey="name"
+                                        cx="40%"  // Adjust the center x-coordinate to give more space for the legend on the right
+                                        cy="50%"
+                                        outerRadius={100}
+                                        fill="#8884d8"
+                                    >
+                                        {mockData.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                        ))}
+                                    </Pie>
+                                    <Tooltip />
+                                    <Legend align="right" verticalAlign="middle" layout="vertical" />
+                                </PieChart>
+
+                            </Box>
+                        </CardContent>
+                    </Card></Grid>
+                    </Grid>
+                  
+                </Grid>
+                <Grid item md={6} xs={12}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>Upcoming Activities</Typography>
+                    {/* Place for upcoming activities */}
                 </Grid>
             </Grid>
-
         </Box>
     );
 }
-
 
 export default WorkoutMain;
