98b52bece507dca6e949e9cad5c817b72cddb23c
Ethan Wong <>
2024-04-13 16:37:52 +0800

    add authorization in apis

--- myFitness/ClientApp/src/components/Profile/UserProfile.js
+++ myFitness/ClientApp/src/components/Profile/UserProfile.js
@@ -1,217 +1,235 @@
 import React, { useState, useEffect } from "react";
 import {
-    Avatar,
-    Divider,
-    Box,
-    Typography,
-    Button,
-    Container,
-    Grid,
-    Card,
-    CardContent,
-    getInputUtilityClass,
+  Avatar,
+  Divider,
+  Box,
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  getInputUtilityClass,
 } from "@mui/material";
-import { useLoading } from '../shared/LoadingContext';
+import { useLoading } from "../shared/LoadingContext";
 import PersonIcon from "@mui/icons-material/Person";
 import EditIcon from "@mui/icons-material/Edit";
 import ProfileForm from "./ProfileForm";
+import { useSelector } from "react-redux";
+import { getAppUserIdSelector } from "../redux/selector";
+import { useCookies } from "react-cookie";
 
 export const UserProfile = () => {
-    const [isEdit, setIsEdit] = useState(null);
-    const [showProfileFrom, setShowProfileForm] = useState(false);
-    const [profileInfo, setProfileInfo] = useState(null);
-    const { setLoading } = useLoading();
+  const [isEdit, setIsEdit] = useState(null);
+  const [showProfileFrom, setShowProfileForm] = useState(false);
+  const [profileInfo, setProfileInfo] = useState(null);
+  const { setLoading } = useLoading();
+  const [cookies] = useCookies();
 
-    const handleOpenProfileForm = () => {
-        if (profileInfo?.profile) {
-            setIsEdit(true);
-        }
-        setShowProfileForm(true);
-    };
+  const jwt = cookies.jwt;
+  const userId = useSelector(getAppUserIdSelector);
 
-    const handleCloseProfileForm = () => {
-        setShowProfileForm(false);
-        setIsEdit(false);
-        fetchProfileDetails();
-    };
-
-    useEffect(() => {
-        setLoading(true);
-        fetchProfileDetails();
-    }, []);
-    const fetchProfileDetails = async () => {
-        const UserId = '6602f019cc013fe8b77e6bc5'; // hardcode first, later use persistent userid
-        try {
-            const response = await fetch(`/api/users/${UserId}`);
-            if (!response.ok) {
-                setLoading(false);
-                setProfileInfo(null);
-                return;
-            }
-            const data = await response.json();
-            console.log(data);
-            if (data === null) {
-                setProfileInfo(null);
-            } else {
-                setProfileInfo(data);
-            }
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching profile:', error);
-            setProfileInfo(null);
-            setLoading(false);
-        }
+  const handleOpenProfileForm = () => {
+    if (profileInfo?.profile) {
+      setIsEdit(true);
     }
-    return (
-        <Box sx={{ marginTop: "1rem", marginBottom: "2rem" }}>
-            <Grid container>
-                <Typography
-                    variant="h6"
-                    sx={{
-                        fontWeight: "bold",
-                        marginRight: "0.5rem",
-                        fontSize: "1.5rem",
-                    }}
-                >
-                    Profile
-                </Typography>
-                <Grid item md={12}>
-                    <Divider
-                        sx={{ padding: "0.5rem 0.75rem", marginBottom: "1.75rem" }}
-                    />
-                </Grid>
-                <Grid item md={1.5}></Grid>
-                <Grid item md={9}>
-                    <Card sx={{ borderRadius: "15px", backgroundColor: "white" }}>
-                        <CardContent>
-                            <Container className="container py-5">
-                                <Grid container justifyContent="center">
-                                    <Grid item md={12} xl={4}>
-                                        <Box textAlign="center">
-                                            <Avatar
-                                                sx={{
-                                                    bgcolor: "#000080",
-                                                    width: 100,
-                                                    height: 100,
-                                                    margin: "0 auto",
-                                                }}
-                                            >
-                                                <PersonIcon sx={{ fontSize: 60 }} />
-                                            </Avatar>
-                                            <Typography variant="h4">{profileInfo?.name}</Typography>
-                                            <Typography
-                                                variant="body2"
-                                                className="text-muted mb-4"
-                                            >
-                                                {profileInfo?.emailAddress}
-                                            </Typography>
-                                            <Button
-                                                variant="contained"
-                                                size="large"
-                                                startIcon={<EditIcon />}
-                                                onClick={() => handleOpenProfileForm(true)}
-                                                sx={{
-                                                    borderRadius: "3rem",
-                                                    marginTop: "1rem",
-                                                    marginBottom: "1rem",
-                                                }}
-                                            >
-                                                {profileInfo?.profile ? "Edit Profile" : "Setup Profile"}
-                                            </Button>
-                                            {
-                                                profileInfo?.profile && (<Grid container>
-                                                    <Grid item md={4}>
-                                                        <Typography variant="h5" gutterBottom>
-                                                            {profileInfo?.profile?.gender}
-                                                        </Typography>
-                                                        <Typography
-                                                            variant="body2"
-                                                            className="small text-muted mb-0"
-                                                        >
-                                                            Gender
-                                                        </Typography>
-                                                    </Grid>
-                                                    <Grid item md={4}>
-                                                        <Typography variant="h5" gutterBottom>
-                                                            {profileInfo?.profile?.height}cm
-                                                        </Typography>
-                                                        <Typography
-                                                            variant="body2"
-                                                            className="small text-muted mb-0"
-                                                        >
-                                                            Height CM
-                                                        </Typography>
-                                                    </Grid>
-                                                    <Grid item md={4}>
-                                                        <Typography variant="h5" gutterBottom>
-                                                            {profileInfo?.profile?.weight}KG
-                                                        </Typography>
-                                                        <Typography
-                                                            variant="body2"
-                                                            className="small text-muted mb-0"
-                                                        >
-                                                            Weight
-                                                        </Typography>
-                                                    </Grid>
-                                                </Grid>)
-                                            }
+    setShowProfileForm(true);
+  };
 
-                                        </Box>
-                                    </Grid>
-                                </Grid>
-                                {
-                                    profileInfo?.profile && (
-                                    <Grid container justifyContent="center">
-                                    <Grid item md={12} xl={12}>
-                                        <Typography
-                                            variant="h6"
-                                            sx={{ fontWeight: "bold", marginTop: "1rem" }}
-                                        >
-                                            Interests
-                                        </Typography>
-                                        <Divider className="mt-0 mb-4" />
-                                        <Grid container spacing={3}>
-                                            <Grid item md={6} xl={6} className="mb-3">
-                                                        <Typography variant="h6"> {profileInfo?.profile?.interests.join(', ')}</Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                    <Grid item md={12} xl={12}>
-                                        <Typography
-                                            variant="h6"
-                                            sx={{ fontWeight: "bold", marginTop: "1rem" }}
-                                        >
-                                            Goals
-                                        </Typography>
-                                        <Divider className="mt-0 mb-4" />
-                                        <Grid container spacing={3}>
-                                            <Grid item md={6} xl={6} className="mb-3">
-                                                        <Typography variant="h6"> {profileInfo?.profile?.goals}</Typography>
-                                            </Grid>
-                                            <Grid item md={6} xl={6} className="mb-3">
-                                            </Grid>
-                                        </Grid>
-                                    </Grid>
-                                        </Grid>
-                                    )}
+  const handleCloseProfileForm = () => {
+    setShowProfileForm(false);
+    setIsEdit(false);
+    fetchProfileDetails();
+  };
 
-                            </Container>
-                        </CardContent>
-                    </Card>
+  useEffect(() => {
+    setLoading(true);
+    fetchProfileDetails();
+  }, []);
+  const fetchProfileDetails = async () => {
+    const headers = { Authorization: "Bearer " + jwt };
+    try {
+      const response = await fetch(`/api/users/${userId}`, { headers });
+      if (!response.ok) {
+        setLoading(false);
+        setProfileInfo(null);
+        return;
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data === null) {
+        setProfileInfo(null);
+      } else {
+        setProfileInfo(data);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      setProfileInfo(null);
+      setLoading(false);
+    }
+  };
+  return (
+    <Box sx={{ marginTop: "1rem", marginBottom: "2rem" }}>
+      <Grid container>
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: "bold",
+            marginRight: "0.5rem",
+            fontSize: "1.5rem",
+          }}
+        >
+          Profile
+        </Typography>
+        <Grid item md={12}>
+          <Divider
+            sx={{ padding: "0.5rem 0.75rem", marginBottom: "1.75rem" }}
+          />
+        </Grid>
+        <Grid item md={1.5}></Grid>
+        <Grid item md={9}>
+          <Card sx={{ borderRadius: "15px", backgroundColor: "white" }}>
+            <CardContent>
+              <Container className="container py-5">
+                <Grid container justifyContent="center">
+                  <Grid item md={12} xl={4}>
+                    <Box textAlign="center">
+                      <Avatar
+                        sx={{
+                          bgcolor: "#000080",
+                          width: 100,
+                          height: 100,
+                          margin: "0 auto",
+                        }}
+                      >
+                        <PersonIcon sx={{ fontSize: 60 }} />
+                      </Avatar>
+                      <Typography variant="h4">{profileInfo?.name}</Typography>
+                      <Typography variant="body2" className="text-muted mb-4">
+                        {profileInfo?.emailAddress}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        size="large"
+                        startIcon={<EditIcon />}
+                        onClick={() => handleOpenProfileForm(true)}
+                        sx={{
+                          borderRadius: "3rem",
+                          marginTop: "1rem",
+                          marginBottom: "1rem",
+                        }}
+                      >
+                        {profileInfo?.profile
+                          ? "Edit Profile"
+                          : "Setup Profile"}
+                      </Button>
+                      {profileInfo?.profile && (
+                        <Grid container>
+                          <Grid item md={4}>
+                            <Typography variant="h5" gutterBottom>
+                              {profileInfo?.profile?.gender}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className="small text-muted mb-0"
+                            >
+                              Gender
+                            </Typography>
+                          </Grid>
+                          <Grid item md={4}>
+                            <Typography variant="h5" gutterBottom>
+                              {profileInfo?.profile?.height}cm
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className="small text-muted mb-0"
+                            >
+                              Height CM
+                            </Typography>
+                          </Grid>
+                          <Grid item md={4}>
+                            <Typography variant="h5" gutterBottom>
+                              {profileInfo?.profile?.weight}KG
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              className="small text-muted mb-0"
+                            >
+                              Weight
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      )}
+                    </Box>
+                  </Grid>
                 </Grid>
-                <Grid item md={1.5}></Grid>
-            </Grid>
-            {showProfileFrom && (
-                <Box>
-                    {isEdit ? (
-                        <ProfileForm open={showProfileFrom} isEdit={isEdit} handleClose={handleCloseProfileForm} profileInfo={profileInfo} />
-                    ) : (
-                            <ProfileForm open={showProfileFrom} isEdit={isEdit} handleClose={handleCloseProfileForm} profileInfo={profileInfo} />
-                    )}
-                </Box>
-            )}
+                {profileInfo?.profile && (
+                  <Grid container justifyContent="center">
+                    <Grid item md={12} xl={12}>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontWeight: "bold", marginTop: "1rem" }}
+                      >
+                        Interests
+                      </Typography>
+                      <Divider className="mt-0 mb-4" />
+                      <Grid container spacing={3}>
+                        <Grid item md={6} xl={6} className="mb-3">
+                          <Typography variant="h6">
+                            {" "}
+                            {profileInfo?.profile?.interests.join(", ")}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item md={12} xl={12}>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontWeight: "bold", marginTop: "1rem" }}
+                      >
+                        Goals
+                      </Typography>
+                      <Divider className="mt-0 mb-4" />
+                      <Grid container spacing={3}>
+                        <Grid item md={6} xl={6} className="mb-3">
+                          <Typography variant="h6">
+                            {" "}
+                            {profileInfo?.profile?.goals}
+                          </Typography>
+                        </Grid>
+                        <Grid item md={6} xl={6} className="mb-3"></Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                )}
+              </Container>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={1.5}></Grid>
+      </Grid>
+      {showProfileFrom && (
+        <Box>
+          {isEdit ? (
+            <ProfileForm
+              open={showProfileFrom}
+              isEdit={isEdit}
+              handleClose={handleCloseProfileForm}
+              profileInfo={profileInfo}
+            />
+          ) : (
+            <ProfileForm
+              open={showProfileFrom}
+              isEdit={isEdit}
+              handleClose={handleCloseProfileForm}
+              profileInfo={profileInfo}
+            />
+          )}
         </Box>
-    );
+      )}
+    </Box>
+  );
 };
 
 export default UserProfile;
--- myFitness/Controllers/UserController.cs
+++ myFitness/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using myFitness.Models;
@@ -25,10 +26,12 @@ namespace myFitness.Controllers
 
         // GET: api/users
         [HttpGet]
+        [Authorize]
         public async Task<List<User>> Get() => await _usersServices.GetAsync();
 
         // GET api/users/{id}
         [HttpGet("{id:length(24)}")]
+        [Authorize]
         public User GetUserByUserId(string id)
         {
             User us = _usersServices.Get(id);
