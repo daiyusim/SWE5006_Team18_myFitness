fcbe7a4562bc9b3099786a975a0994d23c990942
daiyusim <daiyusim@gmail.com>
2024-04-21 01:22:32 +0800

    update dockerfile & code structure

---
+++ myFitness/Startup.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using myFitness.Data;
+using myFitness.Services;
+using myFitness.Utils;
+
+namespace myFitness
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<DatabaseSettings>(
+                Configuration.GetSection("ConnectionStrings"));
+
+            services.AddSingleton<IEventServices, EventServices>();
+            services.AddSingleton<IEventRegistrationServices, EventRegistrationServices>();
+            services.AddSingleton<IUserServices, UserServices>();
+            services.AddSingleton<IProfileServices, ProfileServices>();
+            services.AddSingleton<AuthenticationUtils>();
+
+            services.AddControllersWithViews();
+
+            // Jwt configuration
+            string jwtIssuer = Configuration.GetSection("Jwt:Issuer").Get<string>();
+            string jwtKey = Configuration.GetSection("Jwt:Key").Get<string>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtIssuer,
+                        ValidAudience = jwtIssuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    };
+                });
+
+            services.AddSpaStaticFiles(configuration => {
+                configuration.RootPath = "wwwroot";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa => {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
--- myFitness/Utils/AuthenticationUtils.cs
+++ myFitness/Utils/AuthenticationUtils.cs
@@ -2,6 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace myFitness.Utils
